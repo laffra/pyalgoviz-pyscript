c686a4f89d73e2a6c7c5cef74606c4b6de6311e3
Chris Laffra <laffra@gmail.com>
2025-03-20 09:47:05 +0100

    Improve state handling

--- worker.py
+++ worker.py
@@ -13,81 +13,136 @@ code, find dependencies, and perform code completion.
 # pylint: disable=too-many-arguments
 # pylint: disable=too-many-positional-arguments
 
-from builtins import *
-
 import ast
-import builtins
-import inspect
-import importlib
 import json
-import math
 import os
 import random
 import textwrap
 import traceback
-import types
 import sys
 import time
 
 import polyscript
 
+class Runner():
+    """ Runner class for running Python code. """
 
-viz = []
-log = []
-last_lineno = 0
-lineno = 0
-basic_state = {}
-basic_state.update(globals())
+    def __init__(self, script, visualization):
+        """ Runs the script. """
+        self.viz = []
+        self.log = []
+        self.last_lineno = 0
+        self.lineno = 0
+        self.global_state = self.get_state(self.algo_print)
+        self.viz_state = self.get_state(self.viz_print)
+        self.start = time.time()
+        self.script = script
+        self.visualization = visualization
+        self.run()
 
+    def run(self):
+        """ Runs the script. """
+        try:
+            sys.settrace(self.step)
+            exec(self.script, self.global_state, self.global_state)
+        except Exception as e:
+            print(e)
+            self.exec_error(e)
+        finally:
+            sys.settrace(None)
+        publish("log", self.log[::])
 
-def line(x1, y1, x2, y2, color="black", width=1):
-    """ Draw a line """
-    viz.append(f"l({x1},{y1},{x2},{y2},{repr(color)},{width})")
+    def step(self, frame, event, arg):
+        """ Step function for the tracer. """
+        self.viz.clear()
+        if self.update(frame):
+            try:
+                exec(self.visualization, self.viz_state, self.viz_state)
+            except Exception as e:
+                self.viz_error(e)
+            publish("trace", [ self.lineno, "\n".join(self.viz) ])
+        return self.step
+                
+    def update(self, frame):
+        """ Updates the runner. """
+        if time.time() - self.start > 10:
+            raise TimeoutError("Ran more than 10 seconds")
+        if frame.f_code.co_filename != "<string>":
+            return False
+        self.lineno = frame.f_lineno
+        if self.lineno == self.last_lineno:
+            return False
+        self.last_lineno = self.lineno
+        self.viz_state.update(frame.f_locals)
+        self.viz_state["__lineno__"] = self.lineno
+        return True
+    
+    def viz_error(self, e):
+        """ Handle a visualization error. """
+        tb = traceback.extract_tb(sys.exc_info()[2])
+        error_lineno = tb[-1].lineno
+        self.viz.append(f"error(\"Visualization error at line {error_lineno}: {e}\")")
+    
+    def exec_error(self, e):
+        """ Handle an exec error. """
+        tb = traceback.extract_tb(sys.exc_info()[2])
+        error_lineno = tb[-1].lineno
+        publish("error", [error_lineno, str(e)])
 
+    def line(self, x1, y1, x2, y2, color="black", width=1):
+        """ Draw a line """
+        self.viz.append(f"l({x1},{y1},{x2},{y2},{repr(color)},{width})")
 
-def text(x, y, txt, size=12, font="Arial", color="black"):
-    """ Draw a text """
-    viz.append(f"t({x},{y},{repr(txt)},{size},{repr(font)},{repr(color)})")
+    def text(self, x, y, txt, size=12, font="Arial", color="black"):
+        """ Draw a text """
+        self.viz.append(f"t({x},{y},{repr(txt)},{size},{repr(font)},{repr(color)})")
 
+    def circle(self, x, y, radius, fill="white", border="gray"):
+        """ Draw a circle """
+        self.viz.append(f"c({x},{y},{radius},{repr(fill)},{repr(border)})")
 
-def circle(x, y, radius, fill="white", border="gray"):
-    """ Draw a circle """
-    viz.append(f"c({x},{y},{radius},{repr(fill)},{repr(border)})")
+    def arc(self, x, y, radius, start_angle, end_angle, color='black', width=1):
+        """ Draw an arc """
+        self.viz.append(f"a({x},{y},{radius},{start_angle},{end_angle},{repr(color)},{width})")
 
+    def rect(self, x, y, w, h, fill="white", border="gray"):
+        """ Draw a rectangle """
+        self.viz.append(f"r({x},{y},{w},{h},{repr(fill)},{repr(border)})")
 
-def arc(x, y, radius, startAngle, endAngle, color='black', width=1):
-    """ Draw an arc """
-    viz.append(f"a({x},{y},{radius},{startAngle},{endAngle},{repr(color)},{width})")
+    def viz_print(self, *args):
+        """ The visualization script prints something """
+        self.viz.append(f"p(\"{' '.join(map(str, args))}\")")
 
+    def algo_print(self, *args):
+        """ The algorithm script prints something """
+        msg = " ".join(map(str, args))
+        line_msg = f"Line {self.lineno}: {msg}"
+        self.log.append(line_msg)
 
-def rect(x, y, w, h, fill="white", border="gray"):
-    """ Draw a rectangle """
-    viz.append(f"r({x},{y},{w},{h},{repr(fill)},{repr(border)})")
+    def barchart(self, x, y, w, h, data, highlight=None, fill="black", scale=1):
+        """ Draw a barchart """
+        assert isinstance(data, list), f"Expected a list of numbers, not {type(data)}"
+        self.viz.append(f"b({x},{y},{w},{h},{repr(data)},{highlight},{repr(fill)},{scale})")
 
+    def beep(self, frequency=440, duration=10):
+        """ Sound a beep """
+        self.viz.append(f"s({frequency},{duration})")
 
-def viz_print(*args):
-    """ The visualization script prints something """
-    viz.append(f"p(\"{' '.join(map(str, args))}\")")
-
-orig_print = print
-
-def algo_print(*args):
-    """ The algorithm script prints something """
-    msg = " ".join(map(str, args))
-    line_msg = f"Line {lineno}: {msg}"
-    log.append(line_msg)
-    orig_print(line_msg)
-
-
-def barchart(x, y, w, h, data, highlight=None, fill="black", scale=1):
-    """ Draw a barchart """
-    assert isinstance(data, list), f"Expected a list of numbers, not {type(data)}"
-    viz.append(f"b({x},{y},{w},{h},{repr(data)},{highlight},{repr(fill)},{scale})")
-
-
-def beep(frequency=440, duration=10):
-    """ Sound a beep """
-    viz.append(f"s({frequency},{duration})")
+    def get_state(self, print_function):
+        """ Returns a state to pass to exec. """
+        state = {}
+        state.update(globals())
+        state.update({
+            "line": self.line,
+            "text": self.text,
+            "rect": self.rect,
+            "barchart": self.barchart,
+            "circle": self.circle,
+            "arc": self.arc,
+            "beep": self.beep,
+            "print": print_function,
+        })
+        return state
 
 
 def getsource(function, lines):
@@ -133,6 +188,7 @@ def load_choices():
     }
     publish("choices", choices)
 
+
 def publish(topic, data):
     """ Publishes data to the main process. """
     polyscript.xworker.sync.publish(
@@ -142,83 +198,31 @@ def publish(topic, data):
         data
     )
 
-def get_state(print_function):
-    """ Returns the current state of the worker process. """
-    state = {}
-    state.update(basic_state)
-    state.update({
-        "line": line,
-        "text": text,
-        "rect": rect,
-        "barchart": barchart,
-        "circle": circle,
-        "arc": arc,
-        "beep": beep,
-        "print": print_function,
-    })
-    return state
+
+def load(name):
+    """" Loads the algorithm with the given name. """
+    try:
+        publish("source", load_algorithm(name))
+        publish("related", load_related(name))
+    except Exception:
+        tip = "Press the <b>Load...</b> button to try any of the built-in algorithms."
+        msg = f"Cannot load {name}<p>{tip}"
+        publish("error", [ 1, msg ])
+        traceback.print_exc()
 
 
 def handle_request(sender, topic, request):
     """
     Handles requests received by the worker process.
     """
-    global log, last_lineno
     if topic == "run":
         script, visualization = json.loads(request)
-        try:
-            start = time.time()
-            global_state = get_state(algo_print)
-            viz_state = get_state(viz_print)
-            last_lineno = 0
-
-            def step(frame, event, arg):
-                global viz, lineno, last_lineno
-                viz = []
-                filename = frame.f_code.co_filename
-                if filename != "<string>":
-                    return step
-                lineno = frame.f_lineno
-                if lineno == last_lineno:
-                    return step
-                last_lineno = lineno
-                if time.time() - start > 10:
-                    raise TimeoutError("Ran more than 10 seconds")
-                viz_state.update(frame.f_locals)
-                viz_state["__lineno__"] = lineno
-                try:
-                    exec(visualization, viz_state, viz_state)
-                except Exception as e:
-                    tb = traceback.extract_tb(sys.exc_info()[2])
-                    error_lineno = tb[-1].lineno
-                    viz.append(f"error(\"Visualization error at line {error_lineno}: {e}\")")
-                publish("trace", [ lineno, "\n".join(viz) ])
-                return step
-
-            log = []
-            try:
-                sys.settrace(step)
-                exec(script, global_state, global_state)
-            finally:
-                sys.settrace(None)
-            publish("log", log[::])
-        except Exception as e:
-            tb = traceback.extract_tb(sys.exc_info()[2])
-            error_lineno = tb[-1].lineno
-            publish("error", [error_lineno, str(e)])
-            traceback.print_exc()
+        Runner(script, visualization)
     elif topic == "load":
-        try:
-            name = json.loads(request)
-            publish("source", load_algorithm(name))
-            publish("related", load_related(name))
-        except Exception:
-            tip = "Press the <b>Load...</b> button to try any of the built-in algorithms."
-            msg = f"Cannot load {request}<p>{tip}"
-            publish("error", [ 1, msg ])
-            traceback.print_exc()
+        load(json.loads(request))
     else:
         publish("error", json.dumps(f"Unknown topic: {topic}"))
+
 
 polyscript.xworker.sync.handler = handle_request
 polyscript.xworker.sync.subscribe("Worker", "run", "pyodide-worker")
