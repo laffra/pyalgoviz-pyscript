2ef5da6de3fe2e21e58994804156fe0e6068086b
Chris Laffra <laffra@gmail.com>
2025-03-31 10:25:47 +0200

    Improve tones and layout

--- visualizations/general/fizzbuzz.py
+++ visualizations/general/fizzbuzz.py
@@ -14,19 +14,26 @@ __author = "chris@chrislaffra.com"
 
 def __algorithm():
     def fizzbuzz(x):
-        return ("fizzbuzz" if (x % 15==0) else 
-                "fizz" if x % 3 == 0 else  
-                "buzz" if x % 5 == 0 else 
-                x)
+        return (
+            "fizzbuzz" if (x % 15==0) else 
+            "fizz" if x % 3 == 0 else  
+            "buzz" if x % 5 == 0 else 
+            x
+        )
             
     for x in range(1,101):
         result = fizzbuzz(x)
         print(x, result)
 
 def __visualization():
+    if __lineno__ == 9:
+        if result == "fizzbuzz": beep(300, 1500)    
+        if result == "fizz":     beep(800, 500)    
+        if result == "buzz":     beep(1500, 500) 
+
     def draw(SIZE):
         for n in range(x):
-            px = 30 + round(n/SIZE) * 40
+            px = 30 + round(n//SIZE) * 40
             py = 35 + (n%SIZE) *25
             value = fizzbuzz(n+1)
             color, fb = (("red", "FB") if value == 'fizzbuzz' else
@@ -34,10 +41,5 @@ def __visualization():
                          ("orange", "B") if value == 'buzz' else
                          ("#DDD", value))
             text(px, py, fb, 20, "Arial", color)
-        
-    draw(15)  # try also with 9
-
-    if __lineno__ == 9:
-        if result == "fizzbuzz": beep(1500, 1000)    
-        if result == "fizz": beep(800, 1000)    
-        if result == "buzz": beep(300, 1000)
+    
+    draw(9)  # try also with 15
